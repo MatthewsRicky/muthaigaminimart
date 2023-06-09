ec313dc82d8d3c102f53565cd4d777fc43f3c50a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-09 20:23:36 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { AiOutlineSearch } from "react-icons/ai"
+import { BiMenu } from "react-icons/bi"
 import Image from "next/image";
 
 import Logo from "../public/vercel.svg"
@@ -14,8 +15,52 @@ const Navbar: React.FC<NavbarProps> = ({
 }) => {
   return (
     <div className="w-full">
-      <div className="w-full flex justify-between flex-row gap-x-3 py-6 px-4 bg-light-blue-200">
+      <div className="w-full md:flex lg:flex xl:flex hidden justify-between gap-x-3 py-6 px-4 bg-blue-200 items-center">
         <div className="mmm_logo">
+          <Image 
+            width={80}
+            src={Logo}
+            alt="logo" 
+          />
+        </div>
+        <div className="about_us">
+          About Us
+        </div>
+        <div className="location">
+          Location
+        </div>
+        <div className="whatsapp_orders">
+          Whatsapp Orders
+        </div>
+        <div className="contact">
+          Contacts
+        </div>
+        <div className="search_icon">
+          <AiOutlineSearch 
+            size={26}
+          />
+        </div>
+      </div>
+      <div className="flex justify-between md:hidden lg:hidden xl:hidden gap-x-3 py-6 px-4 bg-blue-200 items-center">
+        <div className="mmm_logo">
+          <Image 
+            width={80}
+            src={Logo}
+            alt="logo" 
+          />
+        </div>
+        <button 
+          onClick={() => {}}
+          className=""
+        >
+          <BiMenu
+            size={40}
+          />
+        </button> 
+      </div>
+      
+      <div className="w-full hidden md:hidden lg:hidden xl:hidden items-center flex-col gap-y-3 py-6 px-4 bg-blue-200">
+        <div className="mmm_logo sm:flex">
           <Image 
             width={80}
             src={Logo}
