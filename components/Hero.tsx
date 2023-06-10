a6a8a78b7083f442da286680279f0bc5ddb29215
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-11 01:17:12 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,11 +6,11 @@ const Hero: React.FC = () => {
     <div className="h-[100vh] relative">
       <div 
         className="hero absolute inset-0 bg-cover bg-center"></div>
-        <div className="relative z-10 flex flex-col justify-center items-center h-full">
+        <div className="relative z-10 flex flex-col justify-center items-center gap-4 text-white h-full w-[]">
           <h1 className="text-3xl font-bold">Welcome to Mutaiga Mini Mart</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nihil suscipit vitae obcaecati eos asperiores accusantium corrupti laborum fugit aspernatur.
           </p>
-          <button className=" bg-blue-500/60 p-4 font-semibold rounded-md w-40 text-white">
+          <button className=" bg-blue-500/60 p-4 font-semibold rounded-md w-40">
             Find Out More
           </button>
       </div>
