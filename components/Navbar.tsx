ecf53961ea95aa90c75777ba19daa5de0d6a7d9a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-10 15:51:10 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,92 +8,31 @@ import Link from "next/navigation";
 import Logo from "../public/vercel.svg"
 import { useState } from "react";
 
-interface NavbarProps {
-  children: React.ReactNode
-}
 
-const Navbar: React.FC<NavbarProps> = ({
-  children
-}) => {
+const Navbar: React.FC = () => {
 
   const [isClicked, setIsClicked] = useState(false)
 
   const toggleNavbar = () => {
     setIsClicked(!isClicked)
-  }
-  return (
-    <div className="w-full">
-      <div id="lg_navbar" className="w-full wrap md:flex lg:flex xl:flex hidden justify-between gap-x-3 py-6 px-4 bg-blue-200 items-center">
-        <div className="mmm_logo hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          <Image 
-            width={80}
-            src={Logo}
-            alt="logo" 
-          />
-        </div>
-        <div className="about_us hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          About Us
-        </div>
-        <div className="location hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          Location
-        </div>
-        <div className="whatsapp_orders hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          Whatsapp Orders
-        </div>
-        <div className="contact hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          Contacts
-        </div>
-        <div 
-          onClick={() => {}}
-          className="search_icon hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          <AiOutlineSearch 
-            size={26}
-          />
-        </div>
-      </div>
-      <div className="flex justify-between md:hidden lg:hidden xl:hidden gap-x-3 py-6 px-4 bg-blue-200 items-center">
-        <div className="mmm_logo">
-          <Image 
-            width={80}
-            src={Logo}
-            alt="logo" 
-          />
-        </div>
-        <div className={`menu-icon ${isClicked ? 'open' : ''}`} onClick={toggleNavbar}>
-        <button
-          id="menu_button"
-        >
-          <BiMenu
-            size={40}
-          />
-        </button> 
-        </div>
-      </div>
-      
-      <div id="sm_navbar" className="w-full hidden md:hidden lg:hidden xl:hidden items-center flex-col gap-y-3 py-6 px-4 bg-blue-200">
-      
-        
-        <div className="about_us hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          About Us
-        </div>
-        <div className="location hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          Location
-        </div>
-        <div className="whatsapp_orders hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          Whatsapp Orders
-        </div>
-        <div className="contact hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          Contacts
-        </div>
-        <div className="search_icon hover:scale-110 hover:text-neutral-600 cursor-pointer">
-          <AiOutlineSearch 
-            size={26}
-          />
-        </div>
-      </div>
-    </div>
-   
-  )
-}
+  };
 
-export default Navbar
+  return (
+    <nav className={`navbar ${isClicked ? 'open' : ''}`}>
+      {/* Your navbar content goes here */}
+      <div className="logo">Your Logo</div>
+      <ul className="nav-links">
+        {/* Add your navigation links here */}
+        <li>About Us</li>
+        <li>Contact</li>
+        <li>WhatsApp Orders</li>
+        <li>Location</li>
+      </ul>
+      <div className="menu-icon" onClick={toggleNavbar}>
+        <i className={`fas ${isClicked ? 'fa-times' : 'fa-bars'}`} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
