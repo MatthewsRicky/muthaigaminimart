6efcc9cd56aa0a5fd8e6044eb527e48aa2d09e52
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-10 16:23:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Hero from '../components/Hero'
+import StoreFeatureCard from '../components/StoreFeatureCard'
+import FaqDisplay from '../components/FaqDisplay'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className='w-full overflow-x-hidden overflow-y-auto flex flex-col bg-teal-200'>
-     
+     <Hero />
+     <StoreFeatureCard />
+     <FaqDisplay />
     </div>
   )
 }
