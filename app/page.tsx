fd0742c28d73d8a17c69320026c148a1a3a7beef
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-10 15:51:35 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import About from './aboutus/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='w-full overflow-x-hidden overflow-y-auto flex flex-col bg-teal-200'>
+      <About />
     </div>
   )
 }
