809be8dc95ee14e44b10b7a73f77f1d4ccf3ea7b
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-11 19:43:51 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 import Logo from "../public/vercel.svg"
 import { useState } from "react";
 
-import About from "../app/pages/About";
+import About from "../app/about/About";
 
 const Navbar = () => {
 
