cdf94ed19b71061886162fce7d7456add87822b1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-11 20:25:50 +0300

    Update StoreFeatureCard.tsx

--- components/StoreFeatureCard.tsx
+++ components/StoreFeatureCard.tsx
@@ -2,9 +2,12 @@
 
 const StoreFeatureCard = () => {
   return (
-    <div>
-      <h1>WELCOME TO MUTHAIGA MINI MART</h1>
-      <p></p>
+    <div className="flex flex-col justify-center items-center gap-8 md:w-[80%] my-20 mx-auto px-10 h-[60vh] overflow-hidden">
+      
+      <h1 className="text-2xl font-semibold">WELCOME TO MUTHAIGA MINI MART</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi, aspernatur. Vero aperiam quo obcaecati. Rem iure distinctio incidunt unde, facere eius nemo quo deleniti adipisci aperiam nulla quos! Maiores aperiam eaque perferendis, eos deleniti vitae, esse blanditiis odio id consequatur nulla! Repellendus nihil dolorum distinctio delectus soluta eaque quod assumenda ratione voluptatum dolor fuga, ullam iste in. Nobis nesciunt nihil neque libero voluptatem possimus, quia recusandae et necessitatibus non, excepturi ratione amet sunt tenetur magni asperiores. Minima tempore enim amet voluptatibus officiis aspernatur quo odit ipsum, quas velit unde provident dolorum fugiat obcaecati? Officia laudantium odio est beatae obcaecati molestias?</p>
+    
+      
     </div>
   )
 }
