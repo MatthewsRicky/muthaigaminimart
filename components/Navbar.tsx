127e994430b022706ceae135e751ea789b0efe3d
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-11 17:41:03 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { AiOutlineSearch } from "react-icons/ai"
-import { BiMenu } from "react-icons/bi"
+import { BiMenu, BiXCircle } from "react-icons/bi"
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,34 +10,41 @@ import { useState } from "react";
 
 import About from "../app/pages/About";
 
+const Navbar = () => {
 
-const Navbar: React.FC = () => {
-
-  const [isClicked, setIsClicked] = useState(false)
-
-  const toggleNavbar = () => {
-    setIsClicked(!isClicked);
-    
-  };
+  const [navbar, setNavbar] = useState(false)
 
   return (
-    <nav className={`navbar ${isClicked ? 'open' : ''}`}>
-    
-      <div className="md:hidden lg:hidden xl:hidden cursor-pointer bg-blue-500/60 p-4 font-semibold flex justify-center rounded-md w-20" onClick={toggleNavbar}>
-            <BiMenu 
-              size={26}
-            />
-      </div>
-     
-      <ul className="w-full hidden md:flex justify-between py-6 px-4 bg-blue-500/60 bg-opacity-60">
-        <li>
-          <div className="logo">
+    <nav className="">
+     <div>
+      <ul className="flex w-full py-6 px-4 bg-blue-500/60 justify-between md:hidden cursor-pointer p-4 font-semibold rounded-md">
+      <li>
             <Image 
               src={Logo}
               alt="logo icon"
               width={100}
             />
-          </div>
+        </li>
+        <li>
+          <button className=" rounded-md w-20" onClick={() => setNavbar(!navbar)}>
+           {navbar ? (
+            <BiXCircle 
+            size={26}
+          />
+           ): <BiMenu 
+           size={26}
+         />} 
+          </button>
+      </li>
+      </ul>
+     </div>
+      <ul className="w-full hidden md:flex justify-between py-6 px-4 bg-blue-500/60 bg-opacity-60">
+        <li>
+            <Image 
+              src={Logo}
+              alt="logo icon"
+              width={100}
+            />
         </li>
         <li>
           <Link href="/">Home</Link>
@@ -55,6 +62,26 @@ const Navbar: React.FC = () => {
           <Link href="/location">Location</Link>
         </li>
       </ul>
+
+      <div className="small_menu hidden flex-col bg-blue-500/60">
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/"><About /></Link>
+          </li>
+          <li>
+            <Link href="/WhatsappOrders">WhatsApp Orders</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/location">Location</Link>
+          </li>
+        </ul>
+      </div>
       
       
     
