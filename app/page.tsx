bec17a5a8882a25b80efc969ec9b267154019798
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-11 19:43:27 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Hero from '../components/Hero'
+import Hero  from '../components/Hero'
 import StoreFeatureCard from '../components/StoreFeatureCard'
 import FaqDisplay from '../components/FaqDisplay'
 
