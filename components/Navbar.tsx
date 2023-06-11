5ddd354695252e84ad2bfd40cf41dad894df1fdc
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-11 19:18:21 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,56 +33,37 @@ const Navbar = () => {
           />
            ): <BiMenu 
            size={26}
-         />} 
+         />
+         
+         } 
           </button>
       </li>
       </ul>
      </div>
-      <ul className="w-full hidden md:flex justify-between py-6 px-4 bg-blue-500/60 bg-opacity-60">
-        <li>
-            <Image 
-              src={Logo}
-              alt="logo icon"
-              width={100}
-            />
-        </li>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/"><About /></Link>
-        </li>
-        <li>
-          <Link href="/WhatsappOrders">WhatsApp Orders</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link href="/location">Location</Link>
-        </li>
-      </ul>
 
-      <div className="small_menu hidden flex-col bg-blue-500/60">
-        <ul>
-          <li>
+     <div className={` md:flex justify-between py-6 px-4 bg-blue-500/60 bg-opacity-60 ${navbar ? 'block' : 'hidden'}`}>
+      <ul className="w-full flex flex-col md:flex-row justify-between items-center  bg-opacity-60">
+        
+          <li className="flex w-full md:w-auto md:flex justify-center md:px-4 py-4 hover:bg-blue-900 hover:bg-opacity-60 hover:border-b-2 border-blue-500 border-opacity-50 rounded-md hover:shadow-md transition">
             <Link href="/">Home</Link>
           </li>
-          <li>
-            <Link href="/"><About /></Link>
+          <li className="flex w-full md:w-auto md:flex justify-center md:px-4 py-4 hover:bg-blue-900 hover:bg-opacity-60 hover:border-b-2 border-blue-500 border-opacity-50 rounded-md hover:shadow-md transition">
+            <Link href="/about">About </Link>
           </li>
-          <li>
-            <Link href="/WhatsappOrders">WhatsApp Orders</Link>
+          <li className="flex w-full md:w-auto md:flex justify-center md:px-4 py-4 hover:bg-blue-900 hover:bg-opacity-60 hover:border-b-2 border-blue-500 border-opacity-50 rounded-md hover:shadow-md transition">
+            <Link href="/whatsapporders">WhatsApp Orders</Link>
           </li>
-          <li>
+          <li className="flex w-full md:w-auto md:flex justify-center md:px-4 py-4 hover:bg-blue-900 hover:bg-opacity-60 hover:border-b-2 border-blue-500 border-opacity-50 rounded-md hover:shadow-md transition">
             <Link href="/contact">Contact</Link>
           </li>
-          <li>
+          <li className="flex w-full md:w-auto md:flex justify-center md:px-4 py-4 hover:bg-blue-900 hover:bg-opacity-60 hover:border-b-2 border-blue-500 border-opacity-50 rounded-md hover:shadow-md transition">
             <Link href="/location">Location</Link>
           </li>
         </ul>
-      </div>
-      
+     </div>
+     
+
+     
       
     
       
