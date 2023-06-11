15b522ae58900e393fc0c72e09ee5d7f0520a313
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-11 16:09:13 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,36 +21,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className={`navbar ${isClicked ? 'open' : (
-      <div className="small_menu">
-        <ul className="w-full hidden md:flex justify-between py-6 px-4 bg-blue-500/60 bg-opacity-60">
-          <li>
-            <div className="logo">
-              <Image 
-                src={Logo}
-                alt="logo icon"
-                width={100}
-              />
-            </div>
-          </li>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/"><About /></Link>
-          </li>
-          <li>
-            <Link href="/WhatsappOrders">WhatsApp Orders</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link href="/location">Location</Link>
-          </li>
-        </ul>
-      </div>
-    )}`}>
+    <nav className={`navbar ${isClicked ? 'open' : ''}`}>
     
       <div className="md:hidden lg:hidden xl:hidden cursor-pointer bg-blue-500/60 p-4 font-semibold flex justify-center rounded-md w-20" onClick={toggleNavbar}>
             <BiMenu 
