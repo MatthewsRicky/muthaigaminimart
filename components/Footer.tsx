985708d13063896b450cc58dc148e379b794de27
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-12 18:42:58 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,19 +6,19 @@ const Footer = () => {
   return (
     <div className="w-full p-4 mt`-6 mx-auto bg-blue-950 text-white">
       <div className="flex flex-col w-full md:flex-row justify-between items-center gap-4">
-        <div className="flex flex-col gap-4 w-full p-2 border-2 border-opacity-40 rounded-md shadow-md shadow-white/40">
+        <div className="flex flex-col items-center justify-center gap-4 w-full p-2 px-2 rounded-md shadow-md shadow-white/40">
           <h1>Muthaiga Mini Mart</h1>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat inventore praesentium distinctio corporis soluta porro.</p>
         </div>
-        <div className="flex flex-col items-center gap-4 w-full p-2 border-2 border-opacity-40 rounded-md shadow-md shadow-white/40">
-          <h1 className="text-2xl">Customer Service</h1>
-          <p>Our customer service team at the head office is  available to help you.
+        <div className="flex flex-col items-center justify-center gap-4 w-full p-2 px-2 rounded-md shadow-md shadow-white/40">
+          <h1 className="text-xl uppercase">Customer Service</h1>
+          <p className="text-centerHip">Our customer service team at the head office is  available to help you.
             Monday-Friday: 8am to 5pm
             Saturday: 8am to 1pm
             Sunday: Closed
           </p>
         </div>
-        <div className="flex flex-col gap-4 w-full p-2 border-2 border-opacity-40 rounded-md shadow-md shadow-white/40">
+        <div className="flex flex-col items-center justify-center gap-4 w-full p-2 px-2 rounded-md shadow-md shadow-white/40">
           <h1>Follow us on social media!</h1>
           <h1 className="flex justify-around items-center">
             <AiOutlineFacebook />
