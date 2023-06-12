022bc30bf4735510a9741c59d94a48c39aaba082
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-12 20:06:29 +0300

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -18,8 +18,40 @@ const Carousel: React.FC<CarouselProps> = ({
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? images.length - 1 : prevSlide -1));
   }
 
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === images.length -1 ? 0 : prevSlide + 1));
+  }
+
   return (
-    <div>Carousel</div>
+    <div className="carousel relative w-full h-96">
+      <div className="slide-container flex transition-transform duration-500 ease-in-out transform">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`slide flex-shrink-0 w-full h-full relative ${
+              index === currentSlide ? 'scale-110 shadow-lg' : ''
+            }`}
+            style={{ backgroundImage: `url(${image})` }}
+          >
+            <div className="caption absolute bottom-2 left-2 bg-blue-800 bg-opacity-80 text-white px-4 py-2 text-xl font-bold transition">
+              {captions[index]}
+            </div>
+          </div>
+        ))}
+      </div>
+      <button
+        className="prev-button absolute top-1/2 transform -translate-y-1/2 left-4 text-2xl text-white bg-blue-800 bg-opacity-80 px-3 py-2 rounded-full transition"
+        onClick={previousSlide}
+      >
+        &lt;
+      </button>
+      <button
+        className="next-button absolute top-1/2 transform -translate-y-1/2 right-4 text-2xl text-white bg-blue-800 bg-opacity-80 px-3 py-2 rounded-full transition"
+        onClick={nextSlide}
+      >
+        &gt;
+      </button>
+    </div>
   )
 }
 
