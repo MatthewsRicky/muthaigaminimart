2a3c97bc8abe7e403aeb6bac4e9c389d5a17c7e0
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-12 09:36:02 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,14 @@
 "use client"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div className="flex justify-between">
+        <h1>Brand</h1>
+        <h1>Useful Links</h1>
+        <h1>Customer Service</h1>
+        <h1>Social Media</h1>
+      </div>
+    </div>
   )
 }
 
