012174f75b2be0e286a4fed4e2d801b15f5ab411
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-13 11:35:22 +0300

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -10,20 +10,32 @@ const slides = [
   {url: '/images/beverages.jpeg'},
   {url: '/images/cheese.jpeg'},
   {url: '/images/mmm1.jpeg'},
-  {url: '/images/mmm2.jpeg'},
+  {url: '/images/mmm3.jpeg'},
 ]
 
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
+
+  const nextSlide = () => {
+    
+  }
+
   return (
-    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
       <div
-        style={{backgroundImage: `url(${slides[0].url})`}} 
+        style={{backgroundImage: `url(${slides[currentIndex].url})`}} 
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
           {/*left arrow*/}
-          <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-blue-500/30 text-white cursor-pointer">
-            <BsChevronCompactLeft size={60}/>
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-blue-500/30 text-white cursor-pointer">
+            <BsChevronCompactLeft onClick={prevSlide} size={60}/>
           </div>
           {/*right arrow*/}
-          <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-blue-500/30 text-white cursor-pointer">
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-blue-500/30 text-white cursor-pointer">
             <BsChevronCompactRight size={60}/>
           </div>
         </div>
