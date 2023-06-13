709ec04c03ba585bbd7494e4dd1fefa27b6e7bfb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-13 11:15:37 +0300

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,9 +1,23 @@
 "use client";
+"use client"
 
+import { useState } from "react";
 
 const Carousel = () => {
+const slides = [
+  {url: '/images/bananas.jpeg'},
+  {url: '/images/beverages.jpeg'},
+  {url: '/images/cheese.jpeg'},
+  {url: '/images/mmm1.jpeg'},
+  {url: '/images/mmm2.jpeg'},
+]
+
   return (
-    <div>Carousel</div>
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
+      <div
+        style={{backgroundImage: `url(${slides[0].url})`}} 
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
+    </div>
   )
 }
 
