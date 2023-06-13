efc53077d2ccf377d4afa281252440ff32179112
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-13 11:25:49 +0300

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -2,6 +2,7 @@
 "use client"
 
 import { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 const Carousel = () => {
 const slides = [
@@ -16,7 +17,16 @@ const slides = [
     <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
       <div
         style={{backgroundImage: `url(${slides[0].url})`}} 
-        className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500">
+          {/*left arrow*/}
+          <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-blue-500/30 text-white cursor-pointer">
+            <BsChevronCompactLeft size={60}/>
+          </div>
+          {/*right arrow*/}
+          <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-blue-500/30 text-white cursor-pointer">
+            <BsChevronCompactRight size={60}/>
+          </div>
+        </div>
     </div>
   )
 }
