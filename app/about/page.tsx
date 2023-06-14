511b22d110fe7e34cefa38b456df17176f2187f8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-14 23:52:44 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,7 +17,7 @@ const About = () => {
               Muthatiga Mini Mart is a beloved destination in the heart of Diani Beach, a stunning coastal paradise. Our mini super market caters to the diverse needs of residents and visitors alike. Offering an extensive range of products and services, from locally sourced fresh produce to gourmet snacks and beach essentials, we strive to meet every customer's needs. With a friendly and inviting atmosphere, Muthatiga Mini Mart is dedicated to providing exceptional customer service, ensuring a convenient and enjoyable shopping experience in the vibrant Diani Beach community.
             </p>
             <Link href="/contact">
-            <button className="bg-orange-500/80 p-4 mt-10 text-2xl md:text-3xl lg:text-4xl scale-75 md:scale-90 lg:scale-100 font-semibold transition hover:scale-105 hover:outline-2 rounded-md w-100">
+            <button className="bg-orange-500/80 p-4 mt-10 text-2xl md:text-3xl lg:text-4xl scale-75 md:scale-90 lg:scale-100 font-semibold transition hover:scale-105 hover:outline-2 rounded-lg w-100">
               Talk to Us
             </button>
             </Link>
