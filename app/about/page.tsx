2831c4b00898f428b1158a55e70a1b3cf04f2c81
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-14 23:51:52 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,14 +10,14 @@ const About = () => {
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
        }}  
-        className="flex flex-col gap-4 items-center h-[75vh] py-20 px-20 w-full bg-cover text-white">
-          <div className="items-center  bg-blue-950/80 flex flex-col px-6 py-4 justify-around gap-8">
+        className="flex flex-col gap-4 items-center py-20 px-20 w-full bg-cover text-white">
+          <div className="items-center bg-blue-950/80 flex flex-col px-6 py-4 justify-around gap-8">
             <h2 className="text-3xl md:text-4xl">Muthatiga Mini Mart</h2>
             <p className="text-2xl md:text-3xl font-semi-bold">
               Muthatiga Mini Mart is a beloved destination in the heart of Diani Beach, a stunning coastal paradise. Our mini super market caters to the diverse needs of residents and visitors alike. Offering an extensive range of products and services, from locally sourced fresh produce to gourmet snacks and beach essentials, we strive to meet every customer's needs. With a friendly and inviting atmosphere, Muthatiga Mini Mart is dedicated to providing exceptional customer service, ensuring a convenient and enjoyable shopping experience in the vibrant Diani Beach community.
             </p>
             <Link href="/contact">
-            <button className="bg-orange-500/80 p-4 mt-10 text-3xl scale-75 md:scale-90 lg:scale-100 font-semibold transition hover:scale-105 hover:outline-2 rounded-md w-100">
+            <button className="bg-orange-500/80 p-4 mt-10 text-2xl md:text-3xl lg:text-4xl scale-75 md:scale-90 lg:scale-100 font-semibold transition hover:scale-105 hover:outline-2 rounded-md w-100">
               Talk to Us
             </button>
             </Link>
