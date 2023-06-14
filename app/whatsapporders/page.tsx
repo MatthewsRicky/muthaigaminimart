c7966baad02ae47dee67beefbe9ed009953aea22
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-14 20:12:17 +0300

    Update page.tsx

--- app/whatsapporders/page.tsx
+++ app/whatsapporders/page.tsx
@@ -4,12 +4,11 @@ import Image from "next/image"
 const WhatsappOrders = () => {
   return (
     <div
-      style={{
-        backgroundImage: `url(${'/images/mmm3.jpeg'})`,
-        backgroundPosition: "center",
-        width: "100vw"
-    }} 
-      className=" Flex w-full justify-around items-center">
+      className="hero bg-cover bg-center bg-no-repeat Flex w-full justify-around items-center">
+        <h1>WhatsApp Orders</h1>
+        <p>
+        You can now order your groceries, household items, electronics and other supermarket shopping via WhatsApp from all our branches countrywide.  The process is quite simple:
+        </p>
     </div>
   )
 }
