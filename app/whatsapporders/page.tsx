6b35a904cfc7312ea1458475a6195dbfe6277509
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-14 20:05:56 +0300

    Update page.tsx

--- app/whatsapporders/page.tsx
+++ app/whatsapporders/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image"
 
 
 const WhatsappOrders = () => {
   return (
-    <div>WhatsappOrders</div>
+    <div
+      style={{
+        backgroundImage: `url(${'/images/mmm3.jpeg'})`,
+        backgroundPosition: "center",
+        width: "100vw"
+    }} 
+      className=" Flex w-full justify-around items-center">
+    </div>
   )
 }
 
