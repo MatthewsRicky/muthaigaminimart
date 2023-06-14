bedb91b0309444a1bd183875bdbe62defc97f970
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-06-14 20:27:27 +0300

    Update page.tsx

--- app/whatsapporders/page.tsx
+++ app/whatsapporders/page.tsx
@@ -3,12 +3,41 @@ import Image from "next/image"
 
 const WhatsappOrders = () => {
   return (
-    <div
-      className="hero bg-cover bg-center bg-no-repeat Flex w-full justify-around items-center">
+    <div>
+      <div className="hero bg-cover bg-center bg-no-repeat Flex w-full justify-around items-center">
         <h1>WhatsApp Orders</h1>
-        <p>
+      </div>
+      <div>
+      <p>
         You can now order your groceries, household items, electronics and other supermarket shopping via WhatsApp from all our branches countrywide.  The process is quite simple:
-        </p>
+      </p>
+      <ul>
+          <li>
+            <p>Send your shopping list to any of the below WhatsApp numbers. Include the item description and quantity.</p>
+          </li>
+          <li>
+            <p>Specify your full names and exact location ( area, road/ street, estate, house/apt number).</p>
+          </li>
+          <li>
+            <p>We will do the picking, packing and you can choose to either collect or we deliver to you via Little Cabs</p>
+          </li>
+          <li>
+            <p>Delivery fees for a 0-15km ksh 50 (Nairobi, Nyali Only), 11-15kms ksh 380.</p>
+          </li>
+        </ul>
+        
+        <h3>
+          Terms and Conditions:
+        </h3>
+        <ul>
+          <li>
+            <p></p>
+          </li>
+          <li>
+            <p></p>
+          </li>  
+        </ul>
+      </div>
     </div>
   )
 }
