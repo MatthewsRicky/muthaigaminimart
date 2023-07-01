b33bb324559e5791c2d212ca56deab8c180bb6e1
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-01 11:16:55 +0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="h-[85vh] flex">
       <div className="hero">
-       <div className="flex flex-col justify-center items-center p-4 mx-auto my-10">Contact</div>
+       <div className="flex flex-col justify-center items-center p-4 mx-auto my-10 w-full text-white text-6xl">Contact</div>
       </div>
     </div>
   )
