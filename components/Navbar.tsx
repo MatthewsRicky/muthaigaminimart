822cd0aab816954fe4f72a58f61bd972ae7f26da
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-02 21:47:21 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <nav className="relative top-0 left-0 right-0 text-blue-950 text-xl font-semibold">
      <div className="absolute z-10 left-0 right-0 top-0">
-      <ul className="absolute flex w-full py-6 px-4 bg-blue-950 justify-between md:hidden cursor-pointer p-4 font-semibold">
+      <ul className="absolute flex w-full py-6 px-4 bg-neutral-100 justify-between md:hidden cursor-pointer p-4 font-semibold">
       <li>
             <Image
               src={Logo}
@@ -41,13 +41,13 @@ const Navbar = () => {
       </ul>
      </div>
 
-     <div className={` md:flex justify-between py-6 px-4  top-0 right-0 left-0 shadow-lg text-neutral-100 ${navbar ? 'block' : 'hidden'}`}>
-      <ul className="w-full flex  md:bg-transparent mt-[4.5rem] md:mt-0 flex-col md:flex-row justify-between items-center transition-all duration-300  bg-opacity-60 ">
+     <div className={` md:flex justify-between py-6 px-4 top-0 right-0 left-0 shadow-lg text-neutral-100 ${navbar ? 'block' : 'hidden'}`}>
+      <ul className="w-full flex  mt-[4.5rem] md:mt-0 flex-col md:flex-row justify-between items-center transition-all duration-300  bg-opacity-60 ">
         
           <li
             onClick={() => setNavbar(!navbar)} 
             >
-            <Link className="flex w-full md:w-auto md:flex justify-center md:px-4 py-4 hover:bg-orange-500/90  hover:bg-opacity-60 hover:border-b-2 border-blue-950 border-opacity-50 p-4 rounded-md hover:shadow-md transition border-b-2 md:border-0 text-blue-950" href="/">Home</Link>
+            <Link className="flex w-full md:w-auto md:flex justify-center md:px-4 py-4 hover:bg-gradient-to-t from-orange-500/60 to-orange-300/60 hover:bg-opacity-60 hover:border-b-2 border-blue-950 border-opacity-50 p-4 rounded-md hover:drop-shadow-xl transition border-b-2 md:border-0 text-blue-950" href="/">Home</Link>
           </li>
           <li
             onClick={() => setNavbar(!navbar)} 
