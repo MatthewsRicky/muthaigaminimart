382b442d44119db51d71c571c654b9227b2c5964
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-03 22:10:16 +0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,8 +3,8 @@
 const Contact = () => {
   return (
     <div className="h-[85vh] flex">
-      <div className="hero">
-        <h1 className="absolute top-1/3 py-4 px-6 rounded-md shadow-md shadow-orange-500/90 text-4xl md:text-6xl lg:text-8xl text-orange-500/90 bg-blue-950/80 mx-[15%] transition-all duration-300">
+      <div className="hero flex justify-center items-center relative">
+        <h1 className="flex justify-center absolute top-[50%] items-center py-4 px-6 rounded-md shadow-md shadow-orange-500/90 text-4xl md:text-6xl lg:text-8xl text-orange-500/90 bg-blue-950/80 mx-auto transition-all duration-300">
           Contact
        </h1>
       </div>
