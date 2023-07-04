c6c7acb37fb0627fd5e91c35e50e8e43abca2ce7
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-04 19:20:56 +0300

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -32,16 +32,16 @@ const slides = [
   }
 
   return (
-    <div className="max-w-full h-[780px] w-full py-16 px-10 relative group">
+    <div className="max-w-full h-[780px] w-full py-16 px-36 relative group">
       <div
         style={{backgroundImage: `url(${slides[currentIndex].url})`}} 
         className="shadow-2xl  w-full h-full rounded-2xl bg-center bg-cover duration-500">
           {/*left arrow*/}
-          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-20 text-2xl rounded-full p-2 bg-blue-900/50 text-orange-500 cursor-pointer">
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-20 text-2xl rounded-full p-2 bg-blue-900/50 text-white cursor-pointer">
             <BsChevronCompactLeft onClick={prevSlide} size={60}/>
           </div>
           {/*right arrow*/}
-          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-20 text-2xl rounded-full p-2 bg-blue-900/50 text-orange-500 cursor-pointer">
+          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-20 text-2xl rounded-full p-2 bg-blue-900/50 text-white cursor-pointer">
             <BsChevronCompactRight onClick={nextSlide} size={60}/>
           </div>
           <div className="hidden group-hover:flex justify-center py-2">
