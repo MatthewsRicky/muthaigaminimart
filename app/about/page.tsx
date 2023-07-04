a52eea481b301f4c47bd2a4d0ae51437e27feeeb
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-04 21:51:55 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,11 +6,11 @@ const About = () => {
   return (
       <div 
       className="text-white w-full">
-        <div className="hero">
-        <h2 className="text-3xl md:text-4xl lg:text-6xl">Muthatiga Mini Mart</h2>
+        <div className="hero relative">
+        <h2 className="absolute top-1/3 py-4 px-6 rounded-md shadow-md shadow-orange-500/90 text-4xl md:text-6xl lg:text-8xl text-orange-500/90 bg-blue-950/80 mx-[15%] transition-all duration-300">Muthatiga Mini Mart</h2>
         </div>
           
-          <div className="">
+          <div className="flex flex-col text-blue-950">
             
             <p className="text-xl md:text-2xl lg:text-3xl font-semi-bold">
               Muthatiga Mini Mart is a beloved destination in the heart of Diani Beach, a stunning coastal paradise. Our mini super market caters to the diverse needs of residents and visitors alike. Offering an extensive range of products and services, from locally sourced fresh produce to gourmet snacks and beach essentials, we strive to meet every customer's needs. With a friendly and inviting atmosphere, Muthatiga Mini Mart is dedicated to providing exceptional customer service, ensuring a convenient and enjoyable shopping experience in the vibrant Diani Beach community.
