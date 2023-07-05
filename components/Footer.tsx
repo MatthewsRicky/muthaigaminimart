2f2d50f189b94463ffd55820cf502c680bce1d28
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-05 09:14:07 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           <h1 className="text-xl uppercase">Muthaiga Mini Mart</h1>
           <p className="text-center p-2">Muthatiga Mini Mart: your one-stop shop for fresh local produce, gourmet snacks, everyday essentials, and beach supplies in Diani Beach.</p>
         </div>
-        <div className="flex flex-col items-center justify-center gap-4 w-full p-2 px-2 rounded-md shadow-sm shadow-white">
+        <div className="flex flex-col items-center justify-center gap-4 w-full p-2 px-2 rounded-md shadow-lg shadow-blue-950/60">
           <h1 className="text-xl uppercase">Customer Service</h1>
           <p className="text-center p-2">Our customer service team at the head office is  available to help you.
             Monday-Friday: 9am to 5pm
@@ -18,7 +18,7 @@ const Footer = () => {
             Sunday: Closed
           </p>
         </div>
-        <div className="flex flex-col justify-center gap-8 w-full px-2 rounded-md shadow-sm shadow-white">
+        <div className="flex flex-col justify-center gap-8 w-full px-2 rounded-md shadow-lg shadow-blue-950/60">
           <h1 className=" text-xl text-center uppercase">Follow us on social media!</h1>
           <h1 className="flex text-4xl py-2 text-orange-500/90 bg-white  rounded m-2 justify-around items-center">
             <AiOutlineFacebook className="hover:bg-orange-500/90 transition-all duration-300 hover:text-white m-4" />
