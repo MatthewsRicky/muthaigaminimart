634913ed67dd66e856606df77d44aa32dabbd2d8
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-05 18:42:08 +0300

    Create ContactForm.tsx

---
+++ components/ContactForm.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useState } from "react";
+
+
+const ContactForm = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [feedback, setFeedback] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Handle form submission here (e.g., send data to server)
+
+    // Reset form fields
+    setFirstName('');
+    setSurname('');
+    setPhoneNumber('');
+    setEmail('');
+    setFeedback('');
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Contact Details:</h1>
+        <p>Mobile: +254 700 000 000</p>
+        <p>Phone: +254 700 000 000</p>
+        <p>Email: mmm@gmail.com</p>
+      </div>
+      <form action="" method="get">
+        <div>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="surname">Surname:</label>
+        <input
+          type="text"
+          id="surname"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input
+          type="tel"
+          id="phoneNumber"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="feedback">Feedback:</label>
+        <textarea
+          id="feedback"
+          value={feedback}
+          onChange={(e) => setFeedback(e.target.value)}
+          required
+        ></textarea>
+      </div>
+      <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default ContactForm
