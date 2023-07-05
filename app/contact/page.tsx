bf77e258203dfe2e9b9fd757d027210288927943
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-05 18:47:28 +0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,13 +3,16 @@ import ContactForm from "@/src/components/ContactForm";
 
 const Contact = () => {
   return (
-    <div className="h-[85vh] flex">
+    <div className="h-[85vh] flex flex-col">
       <div className="hero flex justify-center items-center relative">
         <h1 className="flex justify-center absolute top-[50%] items-center py-4 px-6 rounded-md shadow-md shadow-orange-500/90 text-4xl md:text-6xl lg:text-8xl text-orange-500/90 bg-blue-950/80 mx-auto transition-all duration-300">
           Contact
        </h1>
       </div>
-      <ContactForm />
+      <div className="flex justify-center items-center bg-blue-200/40">
+        <ContactForm />
+      </div>
+      
     </div>
   )
 }
