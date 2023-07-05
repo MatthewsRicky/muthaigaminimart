e4c8908566bb0f7f55aba5b7c6bc97d91d1c3090
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-05 09:17:19 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="w-full p-4 mx-auto bg-blue-200/40 text-blue-950">
       <div className="flex flex-col w-full md:flex-row justify-between items-center gap-4">
-        <div className="flex flex-col items-center justify-center gap-4 w-full p-2 px-2 rounded-md shadow-sm shadow-blue-950">
+        <div className="flex flex-col items-center justify-center gap-4 w-full p-2 px-2 rounded-md shadow-lg shadow-blue-950/60">
           <h1 className="text-xl uppercase">Muthaiga Mini Mart</h1>
           <p className="text-center p-2">Muthatiga Mini Mart: your one-stop shop for fresh local produce, gourmet snacks, everyday essentials, and beach supplies in Diani Beach.</p>
         </div>
@@ -20,10 +20,10 @@ const Footer = () => {
         </div>
         <div className="flex flex-col justify-center gap-8 w-full px-2 rounded-md shadow-lg shadow-blue-950/60">
           <h1 className=" text-xl text-center uppercase">Follow us on social media!</h1>
-          <h1 className="flex text-4xl py-2 text-orange-500/90 bg-white  rounded m-2 justify-around items-center">
-            <AiOutlineFacebook className="hover:bg-orange-500/90 transition-all duration-300 hover:text-white m-4" />
-            <AiOutlineInstagram className="hover:bg-orange-500/90 transition-all duration-300 hover:text-white m-4" />
-            <AiOutlineTwitter className="hover:bg-orange-500/90 transition-all duration-300 hover:text-white m-4" />
+          <h1 className="flex text-4xl py-2 text-blue-950/90 bg-white  rounded m-2 justify-around items-center">
+            <AiOutlineFacebook className="hover:bg-blue-950/90 transition-all duration-300 hover:text-white m-4" />
+            <AiOutlineInstagram className="hover:bg-blue-950/90 transition-all duration-300 hover:text-white m-4" />
+            <AiOutlineTwitter className="hover:bg-blue-950/90 transition-all duration-300 hover:text-white m-4" />
           </h1>
         </div>
       </div>
