9b5f80d5398c18e0bd9bad1477ea0604ed13b55f
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-28 21:49:05 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,13 +6,12 @@ import { motion } from "framer-motion";
 const Hero: React.FC = () => {
 	return (
 		<div className="w-full  my-[5rem] md:my-0 mx-auto relative">
-			<div className="hero absolute inset-0 bg-cover overflow-y-hidden bg-center"></div>
+			<div className="hero absolute inset-0 overflow-y-hidden bg-center"></div>
 			<motion.div
 				initial={{ x: -300 }} // Initial x position (off the screen to the left)
 				animate={{ x: 0 }} // Final x position (centered on the screen)
 				transition={{ duration: 1 }} // Animation duration
-				//style={{ display: "inline-block" }} // Necessary for the animation to work
-				className=" border-blue-100 mt-[4rem] bg-blue-950/60 border-2 border-opacity-40 relative z-10 flex flex-col justify-center p-8 rounded-lg items-center gap-8 text-white"
+				className=" border-blue-100 mt-[4rem] bg-blue-950/80 border-2 border-opacity-40 relative z-10 flex flex-col justify-center p-8 rounded-lg items-center mx-20 md:mx-30 lg:mx-40 gap-8 text-white"
 			>
 				<h1 className="text-3xl md:text-4xl lg:text-6xl font-bold">
 					MUTHAIGA MINI MARKET
