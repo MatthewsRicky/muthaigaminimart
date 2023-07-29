2ec052f7027f8b0fb4952052a3cbf65c94873e9a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-29 23:28:18 +0300

    Update page.tsx

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,9 +1,12 @@
-
-
 const Location = () => {
-  return (
-    <div className="h-[85vh]">Location</div>
-  )
-}
+	return (
+		<div className="hero h-[100vh]">
+			<div className="text-white absolute top-[20%] left-[20%] z-10">
+				PH2J+779, At Diani Shopping Centre, Ukunda, Diani Beach Road, Diani
+				Beach
+			</div>
+		</div>
+	);
+};
 
-export default Location
+export default Location;
