fa6fd00e9670c346d99056eb64caa67a4a5485c9
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-30 19:00:59 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Image from 'next/image'
-import { Inter, Roboto } from 'next/font/google'
-import Hero  from '../components/Hero'
-import StoreFeatureCard from '../components/StoreFeatureCard'
-import FaqDisplay from '../components/FaqDisplay'
+import Image from "next/image";
+import { Inter, Roboto } from "next/font/google";
+import Hero from "../components/Hero";
+import StoreFeatureCard from "../components/StoreFeatureCard";
+import FaqDisplay from "../components/FaqDisplay";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className='w-full overflow-x-hidden overflow-y-auto flex flex-col justify-around h-full'>
-     <Hero />
-     <StoreFeatureCard />
-     <FaqDisplay />
-    </div>
-  )
+	return (
+		<div className="w-full overflow-x-hidden overflow-y-auto flex flex-col justify-around h-full">
+			<Hero />
+			<StoreFeatureCard />
+			<FaqDisplay />
+		</div>
+	);
 }
