d1072e09b4bbed4ee70f88de69570dd0d03fa4ba
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-07-31 10:14:28 +0300

    Update page.tsx

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,7 +1,14 @@
 const Location = () => {
 	return (
-		<div className="hero h-[100vh]">
-			<div className="text-white absolute top-[20%] left-[20%] z-10">
+		<div>
+			<div className="hero h-[100vh]">
+				<div className="text-white absolute top-[20%] left-[20%] z-10">
+					<h1 className="flex justify-center absolute top-[50%] items-center py-4 px-6 rounded-md shadow-md shadow-orange-500/90 text-4xl md:text-6xl lg:text-8xl text-orange-500/90 bg-blue-950/80 mx-auto transition-all duration-300">
+						Location
+					</h1>
+				</div>
+			</div>
+			<div>
 				PH2J+779, At Diani Shopping Centre, Ukunda, Diani Beach Road, Diani
 				Beach
 			</div>
