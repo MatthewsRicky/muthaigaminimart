4b4dbf7a99c8c7da995cac9eb5469c8788806e72
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-01 16:38:02 +0300

    Update ContactDetails.tsx

--- components/ContactDetails.tsx
+++ components/ContactDetails.tsx
@@ -16,61 +16,63 @@ import { PhoneIcon } from "@chakra-ui/icons";
 
 const ContactDetails = () => {
 	return (
-		<Card variant={}>
-			<CardHeader>
-				<Heading size="md">Customer Care</Heading>
-			</CardHeader>
+		<div className="flex justify-center items-center">
+			<Card className="flex justify-center items-center w-[360px] md:w-[720px] lg:w-[1080px]">
+				<CardHeader>
+					<Heading size="md">Customer Care</Heading>
+				</CardHeader>
 
-			<CardBody>
-				<Stack
-					divider={<StackDivider />}
-					spacing="4"
-				>
-					<Box>
-						<Heading
-							size="xs"
-							textTransform="uppercase"
-						>
-							<PhoneIcon />
-						</Heading>
-						<Text
-							pt="2"
-							fontSize="sm"
-						>
-							0700 000 000
-						</Text>
-					</Box>
-					<Box>
-						<Heading
-							size="xs"
-							textTransform="uppercase"
-						>
-							<PhoneIcon />
-						</Heading>
-						<Text
-							pt="2"
-							fontSize="sm"
-						>
-							0700 000 000
-						</Text>
-					</Box>
-					<Box>
-						<Heading
-							size="xs"
-							textTransform="uppercase"
-						>
-							Email
-						</Heading>
-						<Text
-							pt="2"
-							fontSize="sm"
-						>
-							mmm@gmail.com
-						</Text>
-					</Box>
-				</Stack>
-			</CardBody>
-		</Card>
+				<CardBody>
+					<Stack
+						divider={<StackDivider />}
+						spacing="4"
+					>
+						<Box>
+							<Heading
+								size="xs"
+								textTransform="uppercase"
+							>
+								<PhoneIcon />
+							</Heading>
+							<Text
+								pt="2"
+								fontSize="sm"
+							>
+								0700 000 000
+							</Text>
+						</Box>
+						<Box>
+							<Heading
+								size="xs"
+								textTransform="uppercase"
+							>
+								<PhoneIcon />
+							</Heading>
+							<Text
+								pt="2"
+								fontSize="sm"
+							>
+								0700 000 000
+							</Text>
+						</Box>
+						<Box>
+							<Heading
+								size="xs"
+								textTransform="uppercase"
+							>
+								Email
+							</Heading>
+							<Text
+								pt="2"
+								fontSize="sm"
+							>
+								mmm@gmail.com
+							</Text>
+						</Box>
+					</Stack>
+				</CardBody>
+			</Card>
+		</div>
 	);
 };
 
