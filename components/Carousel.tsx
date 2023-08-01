614375bdfee7d88003b4537e0345cc460c4d3172
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-01 16:37:43 +0300

    Update Carousel.tsx

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -5,58 +5,68 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 
 const Carousel = () => {
-const slides = [
-  {url: '/images/bananas.jpeg'},
-  {url: '/images/beverages.jpeg'},
-  {url: '/images/cheese.jpeg'},
-  {url: '/images/mmm1.jpeg'},
-  {url: '/images/mmm3.jpeg'},
-]
+	const slides = [
+		{ url: "/images/bananas.jpeg" },
+		{ url: "/images/beverages.jpeg" },
+		{ url: "/images/cheese.jpeg" },
+		{ url: "/images/mmm1.jpeg" },
+		{ url: "/images/mmm3.jpeg" },
+	];
 
-  const [currentIndex, setCurrentIndex] = useState(0)
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  }
+	const prevSlide = () => {
+		const isFirstSlide = currentIndex === 0;
+		const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+		setCurrentIndex(newIndex);
+	};
 
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length -1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1
-    setCurrentIndex(newIndex);
-  }
+	const nextSlide = () => {
+		const isLastSlide = currentIndex === slides.length - 1;
+		const newIndex = isLastSlide ? 0 : currentIndex + 1;
+		setCurrentIndex(newIndex);
+	};
 
-  const goToSlide = (slideIndex: number) => {
-    setCurrentIndex(slideIndex);
-  }
+	const goToSlide = (slideIndex: number) => {
+		setCurrentIndex(slideIndex);
+	};
 
-  return (
-    <div className="max-w-full h-[780px] w-full py-16 px-36 relative group">
-      <div
-        style={{backgroundImage: `url(${slides[currentIndex].url})`}} 
-        className="shadow-2xl  w-full h-full rounded-2xl bg-center bg-cover duration-500">
-          {/*left arrow*/}
-          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-40 text-2xl rounded-full p-2 bg-blue-900/50 text-white cursor-pointer">
-            <BsChevronCompactLeft onClick={prevSlide} size={50}/>
-          </div>
-          {/*right arrow*/}
-          <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-40 text-2xl rounded-full p-2 bg-blue-900/50 text-white cursor-pointer">
-            <BsChevronCompactRight onClick={nextSlide} size={50}/>
-          </div>
-          <div className="hidden group-hover:flex justify-center py-2">
-            {slides.map((slide, slideIndex) => (
-              <div key={''} className="text-3xl cursor-pointer text-white active:text-white">
-                <RxDotFilled 
-                  key={slideIndex}
-                  onClick={() => goToSlide(slideIndex)} 
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-    </div>
-  )
-}
+	return (
+		<div className="max-w-full h-[780px] w-full py-16 px-24 md:px-36 relative group">
+			<div
+				style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+				className="shadow-2xl  w-full h-full rounded-2xl bg-center bg-cover duration-500"
+			>
+				{/*left arrow*/}
+				<div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-40 text-2xl rounded-full p-2 bg-blue-900/50 text-white cursor-pointer">
+					<BsChevronCompactLeft
+						onClick={prevSlide}
+						size={50}
+					/>
+				</div>
+				{/*right arrow*/}
+				<div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-40 text-2xl rounded-full p-2 bg-blue-900/50 text-white cursor-pointer">
+					<BsChevronCompactRight
+						onClick={nextSlide}
+						size={50}
+					/>
+				</div>
+				<div className="hidden group-hover:flex justify-center py-2">
+					{slides.map((slide, slideIndex) => (
+						<div
+							key={""}
+							className="text-3xl cursor-pointer text-white active:text-white"
+						>
+							<RxDotFilled
+								key={slideIndex}
+								onClick={() => goToSlide(slideIndex)}
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Carousel
+export default Carousel;
