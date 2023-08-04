b00849fc9a83968da2b51e50be4f4b4224f56b65
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-04 21:13:12 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,18 +8,17 @@ import Beer from "../public/images/Beerandwine.png";
 const Hero: React.FC = () => {
 	return (
 		<div className="hero w-full my-[5rem] md:my-0 mx-auto relative">
-			<div className="w-1/2 absolute left-50 inset-0 overflow-y-hidden bg-center"></div>
 			<motion.div
 				initial={{ x: -300 }} // Initial x position (off the screen to the left)
 				animate={{ x: [0, 100, -300] }} // Final x position (centered on the screen)
 				transition={{ duration: [1, 1] }} // Animation duration
-				className="mt-[4rem] relative z-10 flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto gap-8 text-white"
+				className="mt-[4rem] relative z-10 flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto gap-8 text-white mix-blend-color-dodge"
 			>
 				<motion.h1
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1.3, ease: "easeOut" }} // Animation duration
-					className="text-3xl md:text-4xl font-bold"
+					className="text-4xl md:text-4xl font-bold"
 				>
 					MUTHAIGA MINI MARKET
 				</motion.h1>
