6184e13cee7d141f6609366c8b076bb657444c5a
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-04 09:04:44 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,13 +13,13 @@ const Hero: React.FC = () => {
 				initial={{ x: -300 }} // Initial x position (off the screen to the left)
 				animate={{ x: [0, 100, -300] }} // Final x position (centered on the screen)
 				transition={{ duration: [1, 1] }} // Animation duration
-				className=" border-white mt-[4rem] border-2 border-opacity-40 relative z-10 flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto gap-8 text-[#000080]"
+				className=" border-white mt-[4rem] border-2 border-opacity-40 relative z-10 flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto gap-8 text-white"
 			>
 				<motion.h1
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1.3, ease: "easeOut" }} // Animation duration
-					className="text-3xl md:text-3xl font-bold"
+					className="text-3xl md:text-4xl font-bold"
 				>
 					MUTHAIGA MINI MARKET
 				</motion.h1>
@@ -31,14 +31,6 @@ const Hero: React.FC = () => {
 				>
 					Enjoy Shopping
 				</motion.p>
-			</motion.div>
-			<motion.div>
-				<Image
-					src={Beer}
-					alt="beer and wine"
-					width={500}
-					height={500}
-				/>
 			</motion.div>
 		</div>
 	);
