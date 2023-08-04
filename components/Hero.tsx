2726f1b71490aedc3c64ad209bbd2a91764aa535
MatthewsRicky <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-04 22:01:55 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,18 +7,18 @@ import Beer from "../public/images/Beerandwine.png";
 
 const Hero: React.FC = () => {
 	return (
-		<div className="hero w-full my-[5rem] md:my-0 mx-auto relative">
+		<div className="hero w-full my-[5rem] md:my-0 relative">
 			<motion.div
 				initial={{ x: -300 }} // Initial x position (off the screen to the left)
-				animate={{ x: [0, 100, -300] }} // Final x position (centered on the screen)
+				animate={{ x: 0 }} // Final x position (centered on the screen)
 				transition={{ duration: [1, 1] }} // Animation duration
-				className="mt-[4rem] relative z-10 flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto gap-8 text-white mix-blend-color-dodge"
+				className="mt-[4rem] relative flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto text-center gap-8 text-white mix-blend-hard-light"
 			>
 				<motion.h1
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1.3, ease: "easeOut" }} // Animation duration
-					className="text-4xl md:text-4xl font-bold"
+					className="text-3xl  md:text-6xl font-bold"
 				>
 					MUTHAIGA MINI MARKET
 				</motion.h1>
@@ -26,7 +26,7 @@ const Hero: React.FC = () => {
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1.4, ease: "easeOut" }} // Animation duration
-					className="text-lg md:text-2xl lg:text-3xl font-semibold"
+					className="text-3xl md:text-4xl lg:text-3xl font-semibold"
 				>
 					Enjoy Shopping
 				</motion.p>
