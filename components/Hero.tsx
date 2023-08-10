bb7b103a326a156a48501b37ea1f2c6278a1111c
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-10 15:45:40 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,18 +7,18 @@ import Beer from "../public/images/Beerandwine.png";
 
 const Hero: React.FC = () => {
 	return (
-		<div className="hero w-full my-[5rem] md:my-0 relative">
+		<div className="hero w-full my-[5rem] md:my-0 group relative">
 			<motion.div
 				initial={{ x: -300 }} // Initial x position (off the screen to the left)
 				animate={{ x: 0 }} // Final x position (centered on the screen)
-				transition={{ duration: [1, 1] }} // Animation duration
-				className="mt-[4rem] relative flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto text-center gap-8 text-[#fff] mix-blend-hard-light"
+				transition={{ duration: 1 }} // Animation duration
+				className="mt-[4rem] absolute top-[30%] left-10 md:left-[20%] sm:top-20 md:top-16 p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] bg-[#000080]/40 mix-blend-hard-light"
 			>
 				<motion.h1
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
-					transition={{ duration: 1.3, ease: "easeOut" }} // Animation duration
-					className="text-3xl  md:text-6xl font-bold"
+					transition={{ duration: 1.5, ease: "easeOut" }} // Animation duration
+					className="text-3xl  md:text-4xl lg:text-5xl font-bold"
 				>
 					MUTHAIGA MINI MARKET
 				</motion.h1>
