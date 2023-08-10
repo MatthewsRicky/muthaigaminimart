533a89da1628c59b016c4716a7ab5136fa01c768
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-11 02:32:15 +0300

    Update page.tsx

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -17,20 +17,20 @@ import { motion, animate } from "framer-motion";
 const Location = () => {
 	return (
 		<div>
-			<div className="hero relative h-[100vh]">
+			<div className="hero w-full my-[5rem] md:my-0 group relative">
 				<motion.div
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
-					transition={{ duration: [1, 1] }} // Animation duration
-					className="mt-[4rem] relative flex flex-col justify-center p-8 rounded-lg items-center max-w-screen-md mx-auto text-center gap-8 text-[#000080] bg-white/80 mix-blend-hard-light"
+					transition={{ duration: 1 }} // Animation duration
+					className="mt-[4rem] flex flex-col absolute top-[30%] left-[24%] sm:left-[30%] md:left-[24%] sm:top-20 md:top-16 p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] bg-[#000080]/40 mix-blend-hard-light"
 				>
 					<motion.h1
 						initial={{ x: -300 }} // Initial x position (off the screen to the left)
 						animate={{ x: 0 }} // Final x position (centered on the screen)
-						transition={{ duration: 1.3, ease: "easeOut" }} // Animation duration
-						className="absolute z-10  text-3xl  md:text-6xl font-bold"
+						transition={{ duration: 1.5, ease: "easeOut" }} // Animation duration
+						className="text-3xl  md:text-4xl lg:text-5xl font-bold"
 					>
-						Location
+						LOCATION
 					</motion.h1>
 				</motion.div>
 			</div>
