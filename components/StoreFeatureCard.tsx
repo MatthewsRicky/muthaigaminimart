2ab1954c5006ab3487031c04141b96702052ffe8
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-11 10:19:14 +0300

    Update StoreFeatureCard.tsx

--- components/StoreFeatureCard.tsx
+++ components/StoreFeatureCard.tsx
@@ -9,7 +9,7 @@ const StoreFeatureCard = () => {
 				<h1 className="my-10 text-2xl md:text-3xl lg:6xl font-semibold">
 					WELCOME TO MUTHAIGA MINI MARKET
 				</h1>
-				<p className="bg-blue-200/20 md:text-xl lg:4xl p-4 shadow-xl">
+				<p className="bg-blue-200/20 md:text-xl lg:4xl p-4 my-12 shadow-xl">
 					Muthatiga Mini Mart is a beloved destination in the heart of Diani
 					Beach, a stunning coastal paradise. Our mini super market caters to
 					the diverse needs of residents and visitors alike. Offering an
@@ -21,7 +21,7 @@ const StoreFeatureCard = () => {
 					the vibrant Diani Beach community.
 				</p>
 			</div>
-			<div className="flex mx-12 md:min-w-[350px] justify-center items-center flex-col md:flex-row md:row-span-2 gap-8 md:gap-10 flex-wrap">
+			<div className="flex mx-12 md:min-w-[350px] justify-center items-center flex-col md:flex-row mt-10 md:row-span-2 gap-8 md:gap-10 flex-wrap">
 				<ImageCard
 					imageSrc="/images/cheese.jpeg"
 					title="Cheese"
