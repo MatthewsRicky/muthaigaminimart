972682cbc4a3ca5c320dc3b7abb969aa6b2638e0
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-13 23:00:43 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,13 +12,13 @@ const Hero: React.FC = () => {
 				initial={{ x: -300 }} // Initial x position (off the screen to the left)
 				animate={{ x: 0 }} // Final x position (centered on the screen)
 				transition={{ duration: 1 }} // Animation duration
-				className="mt-[4rem] flex flex-col absolute top-[30%] left-[12%] sm:left-[16%] md:left-[24%] sm:top-20 md:top-16 p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] bg-[#000080]/40 mix-blend-hard-light"
+				className="mt-[4rem] flex flex-col absolute top-[30%] left-[12%] sm:left-[16%] md:left-[24%] sm:top-20 md:top-16 p-2 md:p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] bg-[#000080]/40 mix-blend-hard-light"
 			>
 				<motion.h1
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1.5, ease: "easeOut" }} // Animation duration
-					className="text-3xl  md:text-4xl lg:text-5xl font-bold"
+					className="text-2xl  md:text-4xl lg:text-5xl font-bold"
 				>
 					MUTHAIGA MINI MARKET
 				</motion.h1>
@@ -26,7 +26,7 @@ const Hero: React.FC = () => {
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1.4, ease: "easeOut" }} // Animation duration
-					className="text-3xl md:text-4xl lg:text-3xl font-semibold"
+					className="text-xl md:text-4xl lg:text-3xl font-semibold"
 				>
 					Enjoy Shopping
 				</motion.p>
