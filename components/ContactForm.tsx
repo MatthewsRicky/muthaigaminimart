31b96c67c8b5dadccc78ade2915f080490fe6211
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-15 22:12:22 +0300

    Update ContactForm.tsx

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -2,89 +2,90 @@
 
 import { useState } from "react";
 
-
 const ContactForm = () => {
+	const [firstName, setFirstName] = useState("");
+	const [surname, setSurname] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
+	const [email, setEmail] = useState("");
+	const [feedback, setFeedback] = useState("");
 
-  const [firstName, setFirstName] = useState('');
-  const [surname, setSurname] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
-  const [feedback, setFeedback] = useState('');
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		// Handle form submission here (e.g., send data to server)
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Handle form submission here (e.g., send data to server)
+		// Reset form fields
+		setFirstName("");
+		setSurname("");
+		setPhoneNumber("");
+		setEmail("");
+		setFeedback("");
+	};
 
-    // Reset form fields
-    setFirstName('');
-    setSurname('');
-    setPhoneNumber('');
-    setEmail('');
-    setFeedback('');
-  };
+	return (
+		<div>
+			<div className="flex flex-col my-10 gap-4">
+				<h1>Reach us at:</h1>
+				<p>Mobile: +254 700 000 000</p>
+				<p>Phone: +254 700 000 000</p>
+				<p>Email: mmm@gmail.com</p>
+			</div>
+			<form
+				action=""
+				method="get"
+			>
+				<div>
+					<label htmlFor="firstName">First Name:</label>
+					<input
+						type="text"
+						id="firstName"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor="surname">Surname:</label>
+					<input
+						type="text"
+						id="surname"
+						value={surname}
+						onChange={(e) => setSurname(e.target.value)}
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor="phoneNumber">Phone Number:</label>
+					<input
+						type="tel"
+						id="phoneNumber"
+						value={phoneNumber}
+						onChange={(e) => setPhoneNumber(e.target.value)}
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						id="email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor="feedback">Feedback:</label>
+					<textarea
+						id="feedback"
+						value={feedback}
+						onChange={(e) => setFeedback(e.target.value)}
+						required
+					></textarea>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	);
+};
 
-  return (
-    <div>
-      <div className="flex flex-col my-10 gap-4">
-        <h1>Reach us at:</h1>
-        <p>Mobile: +254 700 000 000</p>
-        <p>Phone: +254 700 000 000</p>
-        <p>Email: mmm@gmail.com</p>
-      </div>
-      <form action="" method="get">
-        <div>
-        <label htmlFor="firstName">First Name:</label>
-        <input
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="surname">Surname:</label>
-        <input
-          type="text"
-          id="surname"
-          value={surname}
-          onChange={(e) => setSurname(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="phoneNumber">Phone Number:</label>
-        <input
-          type="tel"
-          id="phoneNumber"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="feedback">Feedback:</label>
-        <textarea
-          id="feedback"
-          value={feedback}
-          onChange={(e) => setFeedback(e.target.value)}
-          required
-        ></textarea>
-      </div>
-      <button type="submit">Submit</button>
-      </form>
-    </div>
-  )
-}
-
-export default ContactForm
+export default ContactForm;
