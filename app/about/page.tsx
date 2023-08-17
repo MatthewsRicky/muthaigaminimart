e2b2579525689311491beb8b627781dd78d78de6
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-17 19:54:01 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,46 +4,46 @@ import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
-
-
+import Logo from "@/src/public/images/g1.png";
 
 const About = () => {
 	return (
 		<div className="text-white w-full">
 			<div className="hero w-full md:my-0 group relative">
-				<div className="flex">
-					<div className="">
-						<Image
-							src={"/public/images/g1.png"}
-							alt="Logo"
-							width={100}
-							height={100}
-						/>
-					</div>
-					<motion.div
+				<motion.div
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1 }} // Animation duration
 					className="mt-[4rem] flex flex-col absolute top-[30%] left-[12%] sm:left-[16%] md:left-[24%] sm:top-20 md:top-16 p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] bg-[#000080]/40 mix-blend-hard-light"
 				>
-					<motion.h1
-						initial={{ x: -300 }} // Initial x position (off the screen to the left)
-						animate={{ x: 0 }} // Final x position (centered on the screen)
-						transition={{ duration: 1.5, ease: "easeOut" }} // Animation duration
-						className="text-3xl  md:text-4xl lg:text-5xl font-bold"
-					>
-						ABOUT
-					</motion.h1>
+					<div className="flex gap-4">
+						<div className="">
+							<Image
+								src={Logo}
+								alt="Logo"
+								width={100}
+								height={100}
+							/>
+						</div>
+						<motion.h1
+							initial={{ x: -300 }} // Initial x position (off the screen to the left)
+							animate={{ x: 0 }} // Final x position (centered on the screen)
+							transition={{ duration: 1.5, ease: "easeOut" }} // Animation duration
+							className="text-3xl  md:text-4xl lg:text-5xl font-bold"
+						>
+							ABOUT
+						</motion.h1>
+					</div>
 				</motion.div>
-				</div>
-				
 			</div>
 			<div className="flex w-full bg-blue-200/20 drop-shadow-2xl text-[#000080] p-2">
 				<motion.h1
 					initial={{ x: -300 }} // Initial x position (off the screen to the left)
 					animate={{ x: 0 }} // Final x position (centered on the screen)
 					transition={{ duration: 1 }} // Animation duration
-				>Muthaiga mini Market <i>></i> About</motion.h1>
+				>
+					Muthaiga mini Market <i>{`>`}</i> About
+				</motion.h1>
 			</div>
 			<div className="flex flex-col justify-center items-center text-blue-950 p-8 m-8 drop-shadow-xl">
 				<p className="text-lg md:text-xl lg:text-2xl font-semi-bold bg-blue-200/40 rounded-xl p-4 ">
@@ -58,8 +58,10 @@ const About = () => {
 					the vibrant Diani Beach community.
 				</p>
 				<Link href="/contact">
-					<button className="bg-[#ff2800]/80 p-4 mt-10
-					text-white text-2xl md:text-3xl lg:text-4xl scale-75 md:scale-90 lg:scale-100 font-semibold transition hover:scale-105 hover:outline-2 rounded-lg w-100">
+					<button
+						className="bg-[#ff2800]/80 p-4 mt-10
+					text-white text-2xl md:text-3xl lg:text-4xl scale-75 md:scale-90 lg:scale-100 font-semibold transition hover:scale-105 hover:outline-2 rounded-lg w-100"
+					>
 						Talk to Us
 					</button>
 				</Link>
