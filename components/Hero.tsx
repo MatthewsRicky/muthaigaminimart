11c0a96f8919435723c3edbaf79bfc2657cddce7
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-18 23:06:09 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,18 +5,19 @@ import { motion, animate } from "framer-motion";
 import Image from "next/image";
 import Beer from "../public/images/Beerandwine.png";
 import Logo from "../public/images/g1.png";
+import { Container } from "postcss";
 
 const Hero: React.FC = () => {
 	return (
-		<div className="hero flex w-full justify-center items-center md:my-0 group relative top-[35%]">
+		<div className="hero flex justify-center items-center w-full group relative">
 			<motion.div
 				initial={{ x: -300 }} // Initial x position (off the screen to the left)
 				animate={{ x: 0 }} // Final x position (centered on the screen)
 				transition={{ duration: 1 }} // Animation duration
 				//className="mt-[4rem] flex flex-col absolute top-[30%] left-0 sm:left-[16%] md:left-[20%] xl:left-[33%] sm:top-20 md:top-16 p-2 md:p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] text-sm bg-[#000080]/40 mix-blend-hard-light"
-				className="text-white "
+				className="text-white flex justify-center items-center"
 			>
-				<div className="flex gap-4">
+				<div className="flex justify-center items-center gap-4">
 					<div className="">
 						<Image
 							src={Logo}
