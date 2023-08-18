433cf9aad460b26801b4b3393e1a79deb2b4aac9
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-18 18:46:07 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,10 +10,11 @@ const Hero: React.FC = () => {
 	return (
 		<div className="hero flex w-full justify-center items-center md:my-0 group relative">
 			<motion.div
-				//initial={{ x: -300 }} // Initial x position (off the screen to the left)
+				initial={{ x: -300 }} // Initial x position (off the screen to the left)
 				animate={{ x: 0 }} // Final x position (centered on the screen)
 				transition={{ duration: 1 }} // Animation duration
-				className="mt-[4rem] flex flex-col absolute top-[30%] left-0 sm:left-[16%] md:left-[20%] xl:left-[33%] sm:top-20 md:top-16 p-2 md:p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] text-sm bg-[#000080]/40 mix-blend-hard-light"
+				//className="mt-[4rem] flex flex-col absolute top-[30%] left-0 sm:left-[16%] md:left-[20%] xl:left-[33%] sm:top-20 md:top-16 p-2 md:p-8 rounded-lg mx-auto text-center gap-8 text-[#fff] text-sm bg-[#000080]/40 mix-blend-hard-light"
+				className="text-white"
 			>
 				<div className="flex gap-4">
 					<div className="">
