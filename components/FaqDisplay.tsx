fde16aa0d736d3c14865b29f225c1f67777d80a2
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-19 16:40:37 +0300

    Update FaqDisplay.tsx

--- components/FaqDisplay.tsx
+++ components/FaqDisplay.tsx
@@ -1,16 +1,14 @@
-"use client"
-import Carousel from './Carousel';
+"use client";
+import Carousel from "./Carousel";
 
 const FaqDisplay: React.FC = () => {
- 
-
-  return (
-    <div>
-      <h1>
-        <Carousel />
-      </h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>
+				<Carousel />
+			</h1>
+		</div>
+	);
 };
 
-export default FaqDisplay
+export default FaqDisplay;
