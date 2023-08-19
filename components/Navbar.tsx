5e1eec86f7bd1deaea01ad2679587793b04fb700
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-19 19:53:40 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 					<Link href="/">
 						<li>
 							<Image
-								className="text-md w-[10rem] flex justify-center hover:border-b-2 border-[#000080] border-opacity-50 p-4 rounded-md hover:shadow-md hover:shadow-[#000080] transition border-b-2 md:border-0 text-[#000080]"
+								className="text-md w-24 flex justify-center hover:border-b-2 border-[#000080] border-opacity-50 p-4 rounded-md hover:shadow-md hover:shadow-[#000080] transition border-b-2 md:border-0 text-[#000080]"
 								src={Logo}
 								alt="logo icon"
 								width={50}
@@ -36,7 +36,7 @@ const Navbar = () => {
 			</div>
 
 			<div
-				className={` md:flex md:flex-row pt-6 pb-2 justify-center items-end top-0 right-0 left-0 shadow-b-lg text-neutral-100 ${
+				className={` md:flex md:flex-row pt-6 pb-2 justify-between items-center top-0 right-0 left-0 shadow-b-lg text-neutral-100 ${
 					navbar ? "block" : "hidden"
 				}`}
 			>
@@ -46,7 +46,7 @@ const Navbar = () => {
 						onClick={() => setNavbar(!navbar)}
 					>
 						<Image
-							className="hidden text-xl w-[10rem] md:flex justify-center md:px-4 py-4 hover:border-b-2 border-[#000080] border-opacity-50 p-4 rounded-md hover:shadow-md hover:shadow-[#000080] transition border-b-2 md:border-0 text-[#000080]"
+							className="hidden text-xl w-[10rem] md:flex justify-center md:px-4 py-4 hover:border-b-2 border-[#000080] border-opacity-50 p-4 rounded-md hover:shadow-md hover:shadow-[#000080]border-b-2 md:border-0 text-[#000080]"
 							src={Logo}
 							alt="logo icon"
 							width={100}
@@ -55,7 +55,7 @@ const Navbar = () => {
 
 					<div className="w-full flex mt-[6.5rem] text-sm md:mt-0 flex-col md:flex-row justify-center  mx-0 items-center transition-all duration-300 gap-4 bg-opacity-60 ">
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 hover:underline-offset-1 p-4 text-blue-950 links"
+							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950 links"
 							href="/about"
 							onClick={() => setNavbar(!navbar)}
 						>
@@ -63,7 +63,7 @@ const Navbar = () => {
 						</Link>
 
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 hover:underline-offset-1 p-4 text-blue-950 links"
+							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950 links"
 							href="/whatsapporders"
 							onClick={() => setNavbar(!navbar)}
 						>
@@ -71,7 +71,7 @@ const Navbar = () => {
 						</Link>
 
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 hover:underline-offset-1 p-4 text-blue-950 links"
+							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950 links"
 							href="/contact"
 							onClick={() => setNavbar(!navbar)}
 						>
@@ -79,7 +79,7 @@ const Navbar = () => {
 						</Link>
 
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 hover:underline-offset-1 p-4 text-blue-950 links"
+							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950 links"
 							href="/location"
 							onClick={() => setNavbar(!navbar)}
 						>
