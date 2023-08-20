6f39930c52f745dd19b50151162d2ddc1b5efc87
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-20 09:08:41 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
 
 					<div className="w-full flex mt-[6.5rem] text-sm md:mt-0 flex-col md:flex-row justify-center  mx-0 items-center transition-all duration-300 gap-4 bg-opacity-60">
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950 after:focus:h-[3px] after:focus:w-[100%] after:focus:bg-[#ff2800] after:focus:absolute after:focus:left-0 after:focus:bottom-0"
+							className=""
 							href="/about"
 							onClick={() => setNavbar(!navbar)}
 						>
@@ -63,15 +63,15 @@ const Navbar = () => {
 						</Link>
 
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950"
-							href="/whatsapporders"
+							className=""
+							href="/about"
 							onClick={() => setNavbar(!navbar)}
 						>
 							WHATSAPP ORDERS
 						</Link>
 
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950"
+							className=""
 							href="/contact"
 							onClick={() => setNavbar(!navbar)}
 						>
@@ -79,7 +79,7 @@ const Navbar = () => {
 						</Link>
 
 						<Link
-							className="flex w-[90vw] md:w-auto md:flex justify-center md:px-4 py-4 p-4 text-blue-950"
+							className=""
 							href="/location"
 							onClick={() => setNavbar(!navbar)}
 						>
