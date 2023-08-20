0489426c46503f4624ccfc711fb13459852dba35
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-20 16:20:53 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Inter, Roboto } from "next/font/google";
 import Hero from "../components/Hero";
 import StoreFeatureCard from "../components/StoreFeatureCard";
 import FaqDisplay from "../components/FaqDisplay";
-import NavigationCard from "../components/NavigationCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +11,6 @@ export default function Home() {
 	return (
 		<div className="w-full overflow-x-hidden overflow-y-auto flex flex-col justify-around h-full">
 			<Hero />
-			{/*<NavigationCard />*/}
 			<div className="flex flex-col lg:flex-row">
 				<StoreFeatureCard />
 				<FaqDisplay />
