3b2d00b040fe481c7beaec7c3bf77f54d190f6e7
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-22 18:57:11 +0300

    Update FaqDisplay.tsx

--- components/FaqDisplay.tsx
+++ components/FaqDisplay.tsx
@@ -1,12 +1,44 @@
 "use client";
-import Carousel from "./Carousel";
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const FaqDisplay: React.FC = () => {
 	return (
-		<div>
-			<h1>
-				<Carousel />
-			</h1>
+		<div className="grid grid-cols-4 gap-2">
+			<div>
+				<Image
+					src={"/public/images/Beerandwine.png"}
+					alt="some image"
+					width={350}
+					height={350}
+				/>
+			</div>
+			<div>
+				<Image
+					src={"bottled_juice.png"}
+					alt="some image"
+					width={350}
+					height={350}
+				/>
+			</div>
+			<div>
+				<Image
+					src={"candy.png"}
+					alt="some image"
+					width={350}
+					height={350}
+				/>
+			</div>
+			<div>
+				<Image
+					src={"sunscreen.png"}
+					alt="some image"
+					width={350}
+					height={350}
+				/>
+			</div>
 		</div>
 	);
 };
