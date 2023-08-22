8a758f757fab8ed73ae4d66c1650b9ff0e2bb98f
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-22 20:37:38 +0300

    Update FaqDisplay.tsx

--- components/FaqDisplay.tsx
+++ components/FaqDisplay.tsx
@@ -7,34 +7,43 @@ import Beer from "@/src/public/images/Beerandwine.png";
 import Bjuice from "@/src/public/images/bottled_juice.png";
 import Candy from "@/src/public/images/candy2.png";
 import Sunscreen from "@/src/public/images/sunscreen.png";
+import { Box, Center } from "@chakra-ui/react";
 
 const FaqDisplay: React.FC = () => {
 	return (
-		<div className="grid grid-cols-4 h-96 gap-2">
-			<div>
+		<div className="grid grid-cols-4 md:grid-cols-2 lg:grid-cols-3 h-96 gap-2">
+			<Box boxSize="sm">
 				<Image
 					src={Beer}
 					alt="Beer and Wine"
+					width={300}
+					height={300}
 				/>
-			</div>
-			<div>
+			</Box>
+			<Box boxSize="sm">
 				<Image
 					src={Bjuice}
 					alt="Bottled Juice"
+					width={300}
+					height={300}
 				/>
-			</div>
-			<div>
+			</Box>
+			<Box boxSize="sm">
 				<Image
 					src={Candy}
 					alt="Candy"
+					width={300}
+					height={300}
 				/>
-			</div>
-			<div>
+			</Box>
+			<Box boxSize="sm">
 				<Image
 					src={Sunscreen}
 					alt="Sunscreen"
+					width={300}
+					height={300}
 				/>
-			</div>
+			</Box>
 		</div>
 	);
 };
