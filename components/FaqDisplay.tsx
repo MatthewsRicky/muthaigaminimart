e987ed3d3165d1f6dd5d3636dade4ed8849272e7
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-22 23:42:48 +0300

    Update FaqDisplay.tsx

--- components/FaqDisplay.tsx
+++ components/FaqDisplay.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { Image } from "@chakra-ui/next-js";
-import { motion } from "framer-motion";
+import { motion, scroll } from "framer-motion";
+import { Box, Center } from "@chakra-ui/react";
 
 import Beer from "@/src/public/images/Beerandwine.png";
 import Bjuice from "@/src/public/images/bottled_juice.png";
 import Candy from "@/src/public/images/candy2.png";
 import Sunscreen from "@/src/public/images/sunscreen.png";
-import { Box, Center } from "@chakra-ui/react";
 
 const FaqDisplay: React.FC = () => {
+	//const [scrollYProgreess] = useScroll();
+
 	return (
-		<div className="flex flex-col m-0 flex-wrap md:flex-row justify-center items-center gap-8 w-full mx-10">
+		<motion.div
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			whileHover={{ backgroundColor: "#ff2800" }}
+			className="flex flex-col m-0 flex-wrap md:flex-row justify-center items-center gap-8 w-full mx-10"
+		>
 			<Box className="flex justify-center items-center">
 				<Image
 					src={Beer}
@@ -44,7 +51,7 @@ const FaqDisplay: React.FC = () => {
 					width={300}
 				/>
 			</Box>
-		</div>
+		</motion.div>
 	);
 };
 
