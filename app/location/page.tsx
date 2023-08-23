5d342215e72387543be3cbcd0144bffb3793fbe2
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-24 02:16:21 +0300

    Update page.tsx

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -105,8 +105,8 @@ const Location = () => {
 				</div>
 				<div className="md:max-w-[450px-750px] lg:max-w-[750px-1080px] xl:w-full items center drop-shadow-lg m-4 bg-blue-200/40 p-2 items-center flex flex-col gap-2">
 					
-					<div className="flex justify-center items-center  md:min-w-[400px]">
-						<Link href={"https://goo.gl/maps/GUtcA74v2rdzTKsq7"}>
+					<div className="flex justify-center items-center  md:min-w-[400px] max-w-[750px]">
+						<Link href={"https://goo.gl/maps/GUtcA74v2rdzTKsq7"} target="_blank">
 							<h1 className="font-semibold">Location Map</h1>
 							<Image
 							src={Map}
