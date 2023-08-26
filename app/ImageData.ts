c5ab5e668ca1d27aa18da9d4d5b7f21f1c4af1a5
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-26 15:07:06 +0300

    Update ImageData.ts

--- app/ImageData.ts
+++ app/ImageData.ts
@@ -1,20 +1,20 @@
-
-
-export default Image = [
-  {
-    img: "/sodas.png",
-    title: "Soft Drinks"
-  },
-  {
-    img: "/coffee.png",
-    title: "Coffee"
-  },
-  {
-    img: "/smokables.png",
-    title: "Tobacco Products"
-  },
-  {
-    img: "/wines.png",
-    title: "Fully stocked Liquor"
-  },
-]
+export default Image = () => {
+	[
+		{
+			img: "/sodas.png",
+			title: "Soft Drinks",
+		},
+		{
+			img: "/coffee.png",
+			title: "Coffee",
+		},
+		{
+			img: "/smokables.png",
+			title: "Tobacco Products",
+		},
+		{
+			img: "/wines.png",
+			title: "Fully stocked Liquor",
+		},
+	];
+};
