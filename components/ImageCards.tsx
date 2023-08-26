14344c0d437840fd07c69312b9485a02d42b1056
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-26 21:00:39 +0300

    Update ImageCards.tsx

--- components/ImageCards.tsx
+++ components/ImageCards.tsx
@@ -1,30 +1,34 @@
 import Image from "next/image";
 
 interface ImageCardProps {
-  imageSrc: string;
-  title: string;
-  description: string;
+	imageSrc: string;
+	title: string;
+	description: string;
 }
 
-const ImageCard: React.FC<ImageCardProps> = ({ imageSrc, title, description }) => {
-  return (
-    <div className="max-w-sm group rounded overflow-hidden shadow-lg transition-scale duration-300 hover:scale-105">
-      <div className="relative">
-        <Image 
-          src={imageSrc} 
-          alt={title} 
-          className="w-full h-auto" 
-          width={1000}
-          height={1000}
-        />
-        <div className="absolute inset-0"></div>
-      </div>
-      <div className="px-6 py-4 hidden group-hover:flex flex-col">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-blue-800 text-base">{description}</p>
-      </div>
-    </div>
-  );
+const ImageCard: React.FC<ImageCardProps> = ({
+	imageSrc,
+	title,
+	description,
+}) => {
+	return (
+		<div className="max-w-sm p-4 m-2 group rounded overflow-hidden shadow-lg transition-scale duration-300">
+			<div className="relative bg-center">
+				<Image
+					src={imageSrc}
+					alt={title}
+					className="w-full h-auto"
+					width={200}
+					height={200}
+				/>
+				<div className="absolute inset-0"></div>
+			</div>
+			<div className="px-6 py-4 hidden group-hover:flex flex-col">
+				<div className="font-bold text-xl mb-2">{title}</div>
+				<p className="text-blue-800 text-base">{description}</p>
+			</div>
+		</div>
+	);
 };
 
 export default ImageCard;
