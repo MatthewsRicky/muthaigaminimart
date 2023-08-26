53d84df502189ca4bf4973101cd03ee9fa06b1ce
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-26 21:40:12 +0300

    Update ImageCards.tsx

--- components/ImageCards.tsx
+++ components/ImageCards.tsx
@@ -13,19 +13,20 @@ const ImageCard: React.FC<ImageCardProps> = ({
 }) => {
 	return (
 		<div className="max-w-sm p-4 m-2 group rounded overflow-hidden shadow-lg transition-scale duration-300">
-			<div className="relative bg-center">
+			<div className="relative">
 				<Image
 					src={imageSrc}
 					alt={title}
-					className="w-full h-auto"
+					className="w-full"
 					width={200}
 					height={200}
 				/>
-				<div className="absolute inset-0"></div>
-			</div>
-			<div className="px-6 py-4 hidden group-hover:flex flex-col">
-				<div className="font-bold text-xl mb-2">{title}</div>
-				<p className="text-blue-800 text-base">{description}</p>
+				<div className="absolute inset-0">
+					<div className="px-6 py-4 hidden group-hover:flex flex-col">
+						<div className="font-bold text-xl mb-2">{title}</div>
+						<p className="text-blue-800 text-base">{description}</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
