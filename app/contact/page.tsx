12cea1d82e3cc56d44aa2224f5491e84ea19341e
OdogwuDisney <111373484+MatthewsRicky@users.noreply.github.com>
2023-08-28 21:53:43 +0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ import ContactDetails from "@/src/components/ContactDetails";
 import { motion, animate } from "framer-motion";
 import Image from "next/image";
 
-import Logo from "/public/images/g1.png";
+import Logo from "../public/images/g1.png";
 
 const Contact = () => {
 	return (
